1abfb6fd307f5f295fd64f16de15c722d81602d2
fywest <fywest2109@hotmail.com>
2019-03-18 23:45:08 +0200

    update app.py

--- week2/news/app.py
+++ week2/news/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import json
+import os
 
 app=Flask(__name__)
 
@@ -7,12 +8,17 @@ app=Flask(__name__)
 helloshiyanlou_dict={}
 helloworld_dict={}
 
+#get a list of files under folder
+files_list=[]
+files_list=os.listdir('../files')
+print(files_list)
+
 #open json file
-with open('./files/helloshiyanlou.json','r') as file:
+with open('../files/helloshiyanlou.json','r') as file:
     helloshiyanlou_dict = json.loads(file.read())
 print (helloshiyanlou_dict)
 
-with open('./files/helloworld.json','r') as file:
+with open('../files/helloworld.json','r') as file:
     helloworld_dict = json.loads(file.read())
 print (helloworld_dict)
 
