c6b574d616ae4c1fda8482ef273423a9c3582c79
fywest <fywest2109@hotmail.com>
2019-03-18 17:20:04 +0200

    update challenge file

--- week2/news/app.py
+++ week2/news/app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+import json
 
 app=Flask(__name__)
+
+#declare dict object
+helloshiyanlou_dict={}
+helloworld_dict={}
+
+#open json file
+with open('./files/helloshiyanlou.json','r') as file:
+    helloshiyanlou_dict = json.loads(file.read())
+print (helloshiyanlou_dict)
+
+with open('./files/helloworld.json','r') as file:
+    helloworld_dict = json.loads(file.read())
+print (helloworld_dict)
 
 @app.route('/')
 def index():
     pass
-
 
 @app.route('/files/<filename>')
 def file(filename):
