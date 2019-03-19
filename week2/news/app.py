38a92ab95ba89631541afe62969df5f01872899f
fywest <fywest2109@hotmail.com>
2019-03-20 00:34:43 +0200

    update linja files

--- week2/news/app.py
+++ week2/news/app.py
@@ -4,28 +4,58 @@ import os
 
 app=Flask(__name__)
 
-#declare dict object
-helloshiyanlou_dict={}
-helloworld_dict={}
 
-#get a list of files under folder
-files_list=[]
-files_list=os.listdir('../files')
-print(files_list)
+books={}
+def get_books():
+    global books
+    #get a list of files under folder
+    files_list=[]
+    files_list=os.listdir('../files')
+    #print(files_list)
+    for file_item in files_list:
+        #open json file
+        with open('../files/'+file_item,'r') as file:
+            book_dict = json.loads(file.read())
+            books[file_item]=book_dict
+   
+titles=[]
+def get_titles():
+    for filename,book in books.items():
+        titles.append(book['title'])
 
-#open json file
-with open('../files/helloshiyanlou.json','r') as file:
-    helloshiyanlou_dict = json.loads(file.read())
-print (helloshiyanlou_dict)
-
-with open('../files/helloworld.json','r') as file:
-    helloworld_dict = json.loads(file.read())
-print (helloworld_dict)
+book=''
+def get_content(filename):
+    get_books()
+    for name,book in books.items():
+        print(name,filename)
+        if filename==name:
+            return book['content']
 
 @app.route('/')
 def index():
-    pass
+    get_books()
+    return render_template('index.html',books=books)
 
+@app.route('/title')
+def index_title():
+    get_books()
+    get_titles()
+    return render_template('index.html',titles=titles)
+    
 @app.route('/files/<filename>')
 def file(filename):
-    pass
+    get_books()
+    book=get_content(filename)
+    print(book)
+    return render_template('index.html',book=book)
+
+if __name__=='__main__':
+    get_books()
+    print('books below')
+    print(books)
+    print('*'*20)
+    get_titles()
+    print(titles)
+    book=get_content('helloworld.json')
+    print(book)
+    app.run()
