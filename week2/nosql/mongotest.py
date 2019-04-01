d14f6f50ea84b632f99d673911ac1aa77b3d5432
fywest <fywest2109@hotmail.com>
2019-04-01 22:35:52 +0300

    add challenge8 mongo tag file

--- week2/nosql/mongotest.py
+++ week2/nosql/mongotest.py
@@ -7,8 +7,8 @@ db = client.louplus
 if __name__ == '__main__':
     user1 = {'user_id':1000,'name':'shiyan','pass':10,'study_time':50}
     user2 = {'user_id':2000,'name':'Lou','pass':15,'study_time':171}
-    db.user.delete_many({'user_id':1000})
-    db.user.delete_one({'user_id':2000})
+#    db.user.delete_many({'user_id':1000})
+#    db.user.delete_one({'user_id':2000})
 
     db.user.insert_one(user1)
     db.user.insert_one(user2)
