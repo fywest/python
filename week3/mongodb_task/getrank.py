31b15141df996497b86aa38d6906415b84c3e89c
fywest <fywest2109@hotmail.com>
2019-04-04 14:48:01 +0300

    update getrank.py

--- week3/mongodb_task/getrank.py
+++ week3/mongodb_task/getrank.py
@@ -3,26 +3,52 @@ import sys
 from pymongo import MongoClient
 
 
+client = MongoClient()
+db = client.shiyanlou
+contests = db.contests
+records = db.contests.find()
+records = {}
+#if records is None:
+#    return "NOTFOUND"
+
 def get_rank(user_id):
-    client = MongoClient()
-    db = client.shiyanlou
-    contests = db.contests
-    records = {}
-    if records is None:
-        return "NOTFOUND"
+    print(recodes)
+    return recodes
     rank=0
     score=0
     submit_time=0
-    rank_list=[]
+    users_list=[]
     users_set = set()
     for record in db.contests.find():
-        rank_dict={}
-        if record['user_id'] in users_set:
-        rank += record['challenge_id']
-        score += record['score']
-        submit_time += record['submit_time']
-        print("rank:{} score:{} submit_time:{}".format(rank,score,submit_time))
-    return rank, score, submit_time
+        dict_tmp={}
+        list_tmp=[]
+        id_tmp=record['user_id']
+        rank=record['challenge_id']
+        score=record['score']
+        time=record['submit_time']
+        dict_tmp[id_tmp]=list_temp
+
+        if id_temp in users_set:
+            for key,value in list_tmp:
+                if key=id_tmp:
+                    value[0]+=rank
+                    value[1]+=score
+                    value[2]+=time
+            
+        else:
+            id_tmp=record['user_id']
+            list_tmp.append(id_tmp)
+            rank=record['challenge_id']
+            list_tmp.append(rank)
+            score=record['score']
+            list_tmp.append(score)
+            time=record['submit_time']
+            list_tmp.append(time)
+            dict_tmp[id_tmp]=list_temp
+            users_set.add(id_tmp)
+            user_list.append(dict_tmp)
+
+    return user_list#rank, score, submit_time
 
 
 if __name__ == '__main__':
