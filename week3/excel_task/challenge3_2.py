45dc153f68534780deab3639b0ba20b21293a6bd
fywest <fywest2109@hotmail.com>
2019-04-07 23:26:05 +0300

    update challenge3_2.py and functional is ok

--- week3/excel_task/challenge3_2.py
+++ week3/excel_task/challenge3_2.py
@@ -27,22 +27,115 @@ def combine():
     '''
 
     wb=load_workbook(filename='courses.xlsx')
-    ws=wb['students']
+    ws1=wb['students']
+    ws2=wb['time']
     list_student=[]
-    for x in range(1,11):
+    for x in range(2,486):
         dict_student={}
-        dict_student['create']=(ws.cell(row=x,column=1)).value
-        dict_student['course']=(ws.cell(row=x,column=2)).value
-        dict_student['study']=(ws.cell(row=x,column=3)).value
-        #print(dict_student)   
+        dict_student['create']=(ws1.cell(row=x,column=1)).value
+        dict_student['course']=(ws1.cell(row=x,column=2)).value
+        dict_student['amount']=(ws1.cell(row=x,column=3)).value
         list_student.append(dict_student)
-    for item in list_student:
-        print(item)
- 
-def split():
-    pass
+
+    list_time=[]
+    for x in range(2,486):
+        dict_time={}
+        dict_time['create']=(ws2.cell(row=x,column=1)).value
+        dict_time['course']=(ws2.cell(row=x,column=2)).value
+        dict_time['study']=(ws2.cell(row=x,column=3)).value
+        list_time.append(dict_time)
+
+    list_combine=[]
+    for x in range(0,484):
+        study=0
+        dict_combine={}
+        dict_student=list_student[x]
+        dict_combine['create']=dict_student['create']
+        dict_combine['course']=dict_student['course']
+        dict_combine['amount']=dict_student['amount']
+        for item in list_time:
+            if item['course']==dict_student['course']:
+                dict_combine['study']=item['study']
+                study=item['study']
+                break
+        dict_combine['study']=study
+        list_combine.append(dict_combine)
+
+    list_combine_time=sorted(list_combine,key=lambda k:k['create'])
+    #for index,item in enumerate(list_combine_time):
+        #print(index,item)
+
+    ws3=wb.create_sheet(title="combine")
+    ws3.cell(row=1,column=1).value="created time"    
+    ws3.cell(row=1,column=2).value="course name"    
+    ws3.cell(row=1,column=3).value="amount student"    
+    ws3.cell(row=1,column=4).value="study time"    
+    for x in range(2,486):
+        dict_combine=list_combine_time[x-2]
+        ws3.cell(row=x,column=1).value=dict_combine['create']    
+        ws3.cell(row=x,column=2).value=dict_combine['course'] 
+        ws3.cell(row=x,column=3).value=dict_combine['amount'] 
+        ws3.cell(row=x,column=4).value=dict_combine['study'] 
+
+    wb.save(filename='combine.xlsx')
+    return list_combine_time
+        
+def split(list_combine_time):
+    
+    wb_2013=Workbook()
+    ws_2013=wb_2013.active
+
+    wb_2014=Workbook()
+    ws_2014=wb_2014.active
+
+    wb_2015=Workbook()
+    ws_2015=wb_2015.active
+
+    wb_2016=Workbook()
+    ws_2016=wb_2016.active
+    
+    year=2013
+    for worksheet in [ws_2013,ws_2014,ws_2015,ws_2016]:
+        worksheet.title=str(year)
+        worksheet.cell(row=1,column=1).value="created time"    
+        worksheet.cell(row=1,column=2).value="course name"    
+        worksheet.cell(row=1,column=3).value="amount student"    
+        worksheet.cell(row=1,column=4).value="study time"    
+        year+=1
+        index_2013,index_2014,index_2015,index_2016=2,2,2,2
+    for x in range(2,486):
+        dict_combine=list_combine_time[x-2]
+        if dict_combine['create'].year==2013:
+            ws_2013.cell(row=index_2013,column=1).value=dict_combine['create']    
+            ws_2013.cell(row=index_2013,column=2).value=dict_combine['course'] 
+            ws_2013.cell(row=index_2013,column=3).value=dict_combine['amount'] 
+            ws_2013.cell(row=index_2013,column=4).value=dict_combine['study'] 
+            index_2013+=1
+        elif dict_combine['create'].year==2014:
+            ws_2014.cell(row=index_2014,column=1).value=dict_combine['create']    
+            ws_2014.cell(row=index_2014,column=2).value=dict_combine['course'] 
+            ws_2014.cell(row=index_2014,column=3).value=dict_combine['amount'] 
+            ws_2014.cell(row=index_2014,column=4).value=dict_combine['study'] 
+            index_2014+=1
+        elif dict_combine['create'].year==2015:
+            ws_2015.cell(row=index_2015,column=1).value=dict_combine['create']    
+            ws_2015.cell(row=index_2015,column=2).value=dict_combine['course'] 
+            ws_2015.cell(row=index_2015,column=3).value=dict_combine['amount'] 
+            ws_2015.cell(row=index_2015,column=4).value=dict_combine['study'] 
+            index_2015+=1
+        elif dict_combine['create'].year==2016:
+            ws_2016.cell(row=index_2016,column=1).value=dict_combine['create']    
+            ws_2016.cell(row=index_2016,column=2).value=dict_combine['course'] 
+            ws_2016.cell(row=index_2016,column=3).value=dict_combine['amount'] 
+            ws_2016.cell(row=index_2016,column=4).value=dict_combine['study'] 
+            index_2016+=1
+        else:
+            print("wow , why here.year should be between 2013 to 2016")
+    wb_2013.save(filename='2013.xlsx')
+    wb_2014.save(filename='2014.xlsx')
+    wb_2015.save(filename='2015.xlsx')
+    wb_2016.save(filename='2016.xlsx')
 
 if __name__ == '__main__':
-    combine()
-    split()
-#d.strftime("%Y/%m/%d %H:%M:%S")
+    list_split=combine()
+    split(list_split)
---
+++ week3/excel_task/excel_4.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+dict_student={'a':1,'b':2}
+dict_student
+dict_list=[]
+dict_list.append(dict_student)
+dict_student={'c':3,'d':4}
+dict_list.append(dict_student)
+dict_list
+dict_tmp=dict_list[0]
+dict_tmp
+from datetime import datetime
+a=datetime.now()
+a
+a.year
