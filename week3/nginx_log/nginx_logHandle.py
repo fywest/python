ee0df5cb2532abb5960122a4fb7778a5a205bcba
fywest <fywest2109@hotmail.com>
2019-04-13 17:29:35 +0300

    flask task add

--- week3/nginx_log/nginx_logHandle.py
+++ week3/nginx_log/nginx_logHandle.py
@@ -13,7 +13,6 @@ def open_parser(filename):
                 r'"(.+)"'
                 )
         parsers = re.findall(pattern,logfile.read())
-
     return parsers
 
 def main():
@@ -22,50 +21,55 @@ def main():
     for item in logs:
         dict_item={}
         dict_item['ip']=item[0]
-#time_str=re.findall(r'(.+)\s',item[1])
-#time=datetime.strptime(time_str[0],'%d/%b/%Y:%H:%M:%S')
         dict_item['time']=item[1]
         dict_item['url']=item[2]
         dict_item['code']=int(item[3])
         list_log.append(dict_item)
 
-#print(list_log)
-        
     ip_dict={}
     ip_dict_max={}
-    max_value=0
+    ip_set=set()
 
     url_dict={}
+    url_dict_max={}
+    url_set=set()
 
-    ip_set=set()
+    max_value=0
+    
     for item in list_log:
-#print(item['time'])
         date_str=re.findall(r'(.+)\s',item['time'])
         date=datetime.strptime(date_str[0],'%d/%b/%Y:%H:%M:%S')
-#print(date)
-#print(date.date())
-#print(datetime(2017,1,11,1,1).date())
-#print(ip_set)
         if date.date() == datetime(2017,1,11,1,1).date():
-#print(item)
             if item['ip'] not in ip_set:
-                ip_dict[item['ip']]=0
+                ip_dict[item['ip']]=1
                 ip_set.add(item['ip'])
-            ip_dict[item['ip']]+=1
-
-
+            else:
+                ip_dict[item['ip']]+=1
+        
+        if item['code']==404:
+            if item['url'] not in url_set:
+                url_dict[item['url']]=1
+                url_set.add(item['url'])
+            else:
+                url_dict[item['url']]+=1
 
     for key,value in ip_dict.items():
-        print (key,value)
+#print (key,value)
         if value>max_value:
             max_value=value
             ip_dict_max[key]=value
+#print(ip_dict_max)        
+    
+    max_value=0
+    for key,value in url_dict.items():
+#print (key,value)
+        if value>max_value:
+            max_value=value
+            url_dict_max[key]=value
+#print(url_dict_max)        
 
-    print(ip_dict_max)        
-
-
-    return ip_dict,url_dict
+    return ip_dict_max,url_dict_max
 
 if __name__=='__main__':
     ip_dict,url_dict = main()
-#print(ip_dict, url_dict)
+    print(ip_dict, url_dict)
