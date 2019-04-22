58cafbc6d64579c80464cc3c0d326f54c4209bf6
fywest <fywest2109@hotmail.com>
2019-04-22 23:52:47 +0300

    challeng 15 add

---
+++ week4/ch15_github.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+import  scrapy
+
+class ShiyanlouCoursesSpider(scrapy.Spider):
+
+    name = 'shiyanlou-courses'
+
+    def start_requests(self):
+        url_tmpl = 'https://github.com/shiyanlou?tab=repositories'
+
+        return (url_tmpl.format(i) for i in range(1,4))
+
+
+    def parse(self, response):
+
+        for course in response.css('div.course-body'):
+            yield{
+                'name': course.css('div.course-name::text').extract_first(),
+                'description': course.css('div.course-desc::text').extract_first(),
+                'type': course.css('div.course-footer span.pull-right::text').extract_first(default='Free'),
+                'student': course.xpath('.//span[contains(@class, "pull-left")]/text()[2]').re_first('[^\d]*(\d+)[^\d]*')
+            }
+
+
---
+++ week4/lab17_scrapy_test.py
@@ -0,0 +1,12 @@
+import scrapy
+
+# url=https://github.com/shiyanlou?tab=repositories
+# response.xpath('//*[@id="user-repositories-list"]/ur/li[1]/div[1]/div[1]/h3/a/text()').extract_first()
+
+# response.xpath('//*[@id="user-repositories-list"]/ur/li/div/div/h3/a/text()').extract()
+
+# response.xpath('//*[@id="user-repositories-list"]/ur/li/div/div/h3/a/text()').re('[a-z]+\-?[a-z]+')
+
+
+
+
