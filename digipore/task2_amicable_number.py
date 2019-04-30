ef2b6fe2f604f0732f1928c0de9badecbc2bb810
fywest <fywest2109@hotmail.com>
2019-04-30 11:29:29 +0300

    update with code check pep8

--- digipore/task2_amicable_number.py
+++ digipore/task2_amicable_number.py
@@ -1,39 +1,33 @@
 #!/usr/bin/env python3
-import sys
-
 
 
 def sum_divisors(num):
-    sum_val=0
-    for i in range(1,num):
-        if (num%i==0):
-            sum_val+=i
-#print('sum value is {0}'.format(sum_val))
+    sum_val = 0
+    for i in range(1, int(num/2+1)):
+        if (num % i == 0):
+            sum_val += i
     return sum_val
 
+
 if __name__ == '__main__':
-#num_range=int(sys.argv[0])
-    val=0
-    sum_val=0
-    user_input=input("input number :")
+
+    val = 0
+    sum_val = 0
     try:
-        val = int(user_input)
-    except ValueError:
+        val = int(input("input number : "))
+    except (ValueError, NameError):
         print("please input integer value")
         exit(1)
 
     print('input value is {0}'.format(val))
-    sum_amicable=0
-    for i in range(1,val):
-        sum_a=0
-        sum_b=0
-        sum_a=sum_divisors(i)
-        sum_b=sum_divisors(sum_a)
-        if sum_b>sum_a and sum_a!=i and sum_b==i:
-            print(sum_a,sum_b,i)
-            sum_amicable+=sum_a+sum_b
+    sum_amicable = 0
+    for i in range(1, val):
+        sum_a = 0
+        sum_b = 0
+        sum_a = sum_divisors(i)
+        sum_b = sum_divisors(sum_a)
+        if sum_b > sum_a and sum_a != i and sum_b == i:
+            # print(sum_a,sum_b,i)
+            sum_amicable += sum_a+sum_b
 
     print('sum of all amicable value are  {0}'.format(sum_amicable))
-
-
-
